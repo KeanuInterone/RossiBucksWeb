9ad01ba0c62f52b2c7337d45202aa0ae59f5db76
Keanu Interone <keanu.interone@gmail.com>
2019-06-29 10:06:44 -0600

    added get_points method

--- app/models/shop.rb
+++ app/models/shop.rb
@@ -2,4 +2,16 @@ class Shop < ApplicationRecord
 	has_and_belongs_to_many :employees, class_name: :User, join_table: "shop_employees"
 	has_and_belongs_to_many :reps, class_name: :User, join_table: "shop_reps"
 	has_many :orders
+
+	def get_points 
+		points = 0
+		self.orders.each do |order|
+			order.products.each do |product|
+				points += product.points
+			end
+		end
+		return points
+	end
+
+
 end
