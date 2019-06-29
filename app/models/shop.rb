1cd3f86ad4229f170b9fb42327ccd57ca20443a0
Keanu Interone <keanu.interone@gmail.com>
2019-06-29 12:48:31 -0600

    design shop show

--- app/models/shop.rb
+++ app/models/shop.rb
@@ -2,16 +2,48 @@ class Shop < ApplicationRecord
 	has_and_belongs_to_many :employees, class_name: :User, join_table: "shop_employees"
 	has_and_belongs_to_many :reps, class_name: :User, join_table: "shop_reps"
 	has_many :orders
+	has_many :sold_products, through: :orders, source: :products
 
-	def get_points 
-		points = 0
-		self.orders.each do |order|
-			order.products.each do |product|
-				points += product.points
-			end
-		end
+	def get_points
+		points = 0	
+		self.sold_products.each do |product| 
+			points += product.points
+		end 
 		return points
 	end
 
+	def get_top_product
+
+		productCountMap = Hash.new
+		self.sold_products.each do |product|
+
+			productCount = productCountMap[product.id]
+			if (productCount) 
+				productCountMap[product.id] = productCount + 1
+			else 
+				productCountMap[product.id] = 1
+
+			end
+
+		end
+
+		maxCount = 0
+		maxProductID = nil
+		productCountMap.each do |key, value|
+
+			if value > maxCount 
+				maxCount = value
+				maxProductID = key
+			end
+
+		end
+
+		if maxProductID.nil? 
+			return nil
+		else 
+			return {"product" => Product.find(maxProductID), "count" => maxCount}
+		end
+
+	end
 
 end
